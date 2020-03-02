b60731902e3891fb258db5fdbb7561441430f169
Ubuntu-18-04 <motolegacy.git@gmail.com>
2020-03-02 14:00:03 +0000

    start of no-render arg

--- src/engine/main.c
+++ src/engine/main.c
@@ -1,10 +1,23 @@
+#include <string.h>
+
+#include "types.h"
 #include "window.h"
 #include "game.h"
 #include "version.h"
 
+bool OPT_NORENDER = FALSE;
+
 int main(int argc, char* argv[]) {
+    // Parse command line arguments
+    for(int i = 0; i <= (argc - 1); i++) {
+        if (strcmp("-no-render", argv[i]) == 0) {
+            OPT_NORENDER = TRUE;
+        }
+    }
+
     // Initialize Game window
-    Window_Initialize(I_GAME_WIDTH, I_GAME_HEIGHT, I_GAME_TITLE, argc, argv);
+    if(!OPT_NORENDER)
+        Window_Initialize(I_GAME_WIDTH, I_GAME_HEIGHT, I_GAME_TITLE, argc, argv);
 
     // Init Game handler
     Game_Initialize();
